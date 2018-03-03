efd3802a36867a8cf98e818b6425392cde27c521
rreinold <rreinold@gmail.com>
2018-03-03 15:32:53 -0600

    Added detail modal

--- app.js
+++ app.js
@@ -7,43 +7,49 @@ var CARD_TEMPLATE =
               <p>by {{authors}}</p>\
             </div>\
             <div class="card-action">\
-              <a class="white-text" href="#">Notes</a>\
+              <button class="white black-text btn waves-effect waves-light" onclick="openNotes(\'{{title}}\',\'{{authors}}\')">Notes\
+			    \
+			  </button>\
             </div>\
           </div>\
         </div>'
 var ROW_TEMPLATE = '<div class="row">{{cards}}</div>'
 var BOOKS_PER_ROW = 4
 function startup(){
-	var books = [{"title":"Outliers","authors":["Malcolm Gladwell"]},{"title":"Creativity Inc.","authors":["Edwin Catmull"]},{"title":"The Lean Startup","authors":["Eric Ries"]},{"title":"You Can Negotiate Anything","authors":["Herb Cohen"]},{"title":"Learn to Earn","authors":["Peter Lynch"]},{"title":"How to Invest $50-$5","authors":["000"]},{"title":"Thinking Fast and Slow","authors":["Daniel Kahneman  "]},{"title":"The Art of War","authors":["Sun Tzu"]},{"title":"Zero to One","authors":["Peter Thiel"]},{"title":"The Toyota Way","authors":["Jeffrey K. Liker"]},{"title":"How To Win Friends And Influence People","authors":["Dale Carnegie"]},{"title":"Good to Great","authors":["Jim Collins  "]},{"title":"Great","authors":["Choice"]},{"title":"Four Hour Workweek","authors":["Tim Ferriss"]},{"title":"The Zero Marginal Cost Society","authors":["Jeremy Rifkin"]},{"title":"The Challenger Sale","authors":["Matthew Dixon"]},{"title":"The Way of the Shepherd: 7 Ancient Secrets to Managing Productive People","authors":["Dr. Kevin Leman"]},{"title":"The Total Money Makeover","authors":["Dave Ramsey"]},{"title":"The Power of Habit","authors":["Charles Duhigg"]},{"title":"Sprint","authors":["Braden Kowitz"]},{"title":"Venture Deals","authors":["Jason Mendelson"]},{"title":"The Secret","authors":["Rhonda Byrne "]},{"title":"What Every Body Is Saying","authors":["Joe Navarro"]},{"title":"The Art of Conflict Management","authors":["Michael Dues"]},{"title":"The Millionaire Next Door","authors":["Thomas J. Stanley"]},{"title":"Curious: The Desire to Know and Why Your Future Depends On It","authors":["Ian Leslie"]},{"title":"Linchpin","authors":["Seth Godin "]},{"title":"The Alchemist","authors":["Paulo Coelho"]}]
 
+	$('.modal').modal();
 
-	
-	var booksDiv = document.getElementById('books')
-	var rowOfBooks = []
-	var BOOK_WIDTH = 12 / BOOKS_PER_ROW
-	output = []
-	for(i in books){
-		i=+i
-		var b = books[i]
-		var color = getRandomMateralizeColor()
-		singleBook = CARD_TEMPLATE
-						.replace(/{{title}}/,b.title)
-						.replace(/{{authors}}/,b.authors.join(','))
-						.replace(/{{description}}/,b.description)
-						.replace(/{{color}}/,color)
-						.replace(/{{width}}/g,BOOK_WIDTH)
-		rowOfBooks.push(singleBook)
+	fetchJSON("books.json",renderBooks)
 
-		if( (i+1) % BOOKS_PER_ROW == 0 || i == books.length - 1){
-			var row = ROW_TEMPLATE.replace(/{{cards}}/,rowOfBooks.join(''))
-			output.push(row)
+	function renderBooks(books){
+		var booksDiv = document.getElementById('books')
+		var rowOfBooks = []
+		var BOOK_WIDTH = 12 / BOOKS_PER_ROW
+		output = []
+		var pastReads = books.past
+		for(i in pastReads){
+			i=+i
+			var b = pastReads[i]
+			var color = getRandomMateralizeColor()
+			singleBook = CARD_TEMPLATE
+							.replace(/{{title}}/g,b.title)
+							.replace(/{{authors}}/g,b.authors.join(','))
+							.replace(/{{description}}/g,b.description)
+							.replace(/{{color}}/g,color)
+							.replace(/{{width}}/g,BOOK_WIDTH)
+			rowOfBooks.push(singleBook)
 
-			// clean for next row
-			rowOfBooks = []
+			if( (i+1) % BOOKS_PER_ROW == 0 || i == pastReads.length - 1){
+				var row = ROW_TEMPLATE.replace(/{{cards}}/,rowOfBooks.join(''))
+				output.push(row)
+
+				// clean for next row
+				rowOfBooks = []
+			}
 		}
-	}
 
-	booksDiv.innerHTML = output.join('')
+		booksDiv.innerHTML = output.join('')
+	}
 }
 
 function getDetailed(title, author, callback){
@@ -52,21 +58,29 @@ function getDetailed(title, author, callback){
 	targetURL = GOOGLE_BOOKS_TEMPLATE_URL
 			.replace("{{title}}",titleSafe)
 			.replace("{{author}}", authorSafe)
-	var xhttp = new XMLHttpRequest();
-  	xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    	try{
-    		callback(JSON.parse(this.responseText))
-    	}
-    	catch(e){
-    		console.log("Unable to process response: " + this.responseText)
-    		callback({})
-    	}
-    }
-	};
-	xhttp.open("GET", targetURL, true);
-	xhttp.send();
 
+	fetchJSON(targetURL, callback)
+
+}
+
+function openNotes(title, author){
+	getDetailed(title, author, function(details){
+		// TODO Check items == 0
+		if(details.items.length === 0){
+			var error = "Cannot find book in Google Book store"
+			console.log(error)
+			document.getElementById("modalHeader").innerHTML = error
+			$('#modal1').modal('open')
+			return;
+		}
+		bookInfo = details.items[0].volumeInfo
+		var imageURL = bookInfo.imageLinks.thumbnail
+		var description = bookInfo.description || ""
+		document.getElementById("bookCover").src = imageURL
+		document.getElementById("modalHeader").innerHTML = title
+		document.getElementById("modalDescription").innerHTML = description
+		$('#modal1').modal('open');
+	})
 }
 
 function getRandomMateralizeColor(){
@@ -80,3 +94,20 @@ function getRandomMateralizeColor(){
 	var intensity = values.intensity[Math.floor(Math.random()*values.intensity.length)]
 	return color + " " + brightness + intensity
 }
+
+function fetchJSON(url, callback){
+		var xhttp = new XMLHttpRequest();
+	  	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	    	try{
+	    		callback(JSON.parse(this.responseText))
+	    	}
+	    	catch(e){
+	    		console.log("Unable to process response: " + this.responseText)
+	    		callback({})
+	    	}
+	    }
+		};
+		xhttp.open("GET", url, true);
+		xhttp.send();
+	}
