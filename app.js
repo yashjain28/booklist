f2c4e126f7c08915a500ccd2a5b93600bb5ee9e1
Yash Jain <yjain@clearblade.com>
2020-04-04 23:00:33 -0500

    seaparated the past and future list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var GOOGLE_BOOKS_TEMPLATE_URL = "https://www.googleapis.com/books/v1/volumes\?q\={{title}}+inauthor:{{author}}"
-var CARD_TEMPLATE = 
-        '<div class="col s{{width}} m{{width}}">\
+var GOOGLE_BOOKS_TEMPLATE_URL =
+  "https://www.googleapis.com/books/v1/volumes?q={{title}}+inauthor:{{author}}";
+var CARD_TEMPLATE =
+  '<div class="col s{{width}} m{{width}}">\
           <div class="card {{color}}">\
             <div class="card-content white-text">\
               <span class="card-title">{{title}}</span>\
@@ -12,102 +13,122 @@ var CARD_TEMPLATE =
 			  </button>\
             </div>\
           </div>\
-        </div>'
-var ROW_TEMPLATE = '<div class="row">{{cards}}</div>'
-var BOOKS_PER_ROW = 4
-function startup(){
+        </div>';
+var ROW_TEMPLATE = '<div class="row">{{cards}}</div>';
+var BOOKS_PER_ROW = 4;
+function startup() {
+  $(".modal").modal();
 
-	$('.modal').modal();
+  fetchJSON("books.json", renderBooks);
 
-	fetchJSON("books.json",renderBooks)
+  function renderBooks(books) {
+    let booksDiv = document.getElementById("books");
+    let futureBooksDiv = document.getElementById("futurebooks");
 
-	function renderBooks(books){
-		var booksDiv = document.getElementById('books')
-		var rowOfBooks = []
-		var BOOK_WIDTH = 12 / BOOKS_PER_ROW
-		output = []
-		var pastReads = books.past
-		for(i in pastReads){
-			i=+i
-			var b = pastReads[i]
-			var color = getRandomMateralizeColor()
-			singleBook = CARD_TEMPLATE
-							.replace(/{{title}}/g,b.title)
-							.replace(/{{authors}}/g,b.authors.join(','))
-							.replace(/{{description}}/g,b.description)
-							.replace(/{{color}}/g,color)
-							.replace(/{{width}}/g,BOOK_WIDTH)
-			rowOfBooks.push(singleBook)
+    let pastReadCards = generateCards(books.past);
+	let futureReadCards = generateCards(books.future);
+	
+	booksDiv.innerHTML = pastReadCards.join("");
+    futureBooksDiv.innerHTML = futureReadCards.join("");
+  }
 
-			if( (i+1) % BOOKS_PER_ROW == 0 || i == pastReads.length - 1){
-				var row = ROW_TEMPLATE.replace(/{{cards}}/,rowOfBooks.join(''))
-				output.push(row)
+  function generateCards(reads) {
+    let rowOfBooks = [];
+    let BOOK_WIDTH = 12 / BOOKS_PER_ROW;
+    let output = [];
+    for (i in reads) {
+      i = +i;
+      let b = reads[i];
+      let color = getRandomMateralizeColor();
+      singleBook = CARD_TEMPLATE.replace(/{{title}}/g, b.title)
+        .replace(/{{authors}}/g, b.authors.join(","))
+        .replace(/{{description}}/g, b.description)
+        .replace(/{{color}}/g, color)
+        .replace(/{{width}}/g, BOOK_WIDTH);
+      rowOfBooks.push(singleBook);
 
-				// clean for next row
-				rowOfBooks = []
-			}
-		}
+      if ((i + 1) % BOOKS_PER_ROW == 0 || i == reads.length - 1) {
+        let row = ROW_TEMPLATE.replace(/{{cards}}/, rowOfBooks.join(""));
+        output.push(row);
 
-		booksDiv.innerHTML = output.join('')
-	}
+        // clean for next row
+        rowOfBooks = [];
+      }
+    }
+    return output;
+  }
 }
 
-function getDetailed(title, author, callback){
-	var authorSafe = encodeURIComponent(author)
-	var titleSafe = encodeURIComponent(title)
-	targetURL = GOOGLE_BOOKS_TEMPLATE_URL
-			.replace("{{title}}",titleSafe)
-			.replace("{{author}}", authorSafe)
+function getDetailed(title, author, callback) {
+  var authorSafe = encodeURIComponent(author);
+  var titleSafe = encodeURIComponent(title);
+  targetURL = GOOGLE_BOOKS_TEMPLATE_URL.replace("{{title}}", titleSafe).replace(
+    "{{author}}",
+    authorSafe
+  );
 
-	fetchJSON(targetURL, callback)
-
+  fetchJSON(targetURL, callback);
 }
 
-function openNotes(title, author){
-	getDetailed(title, author, function(details){
-		// TODO Check items == 0
-		if(details.items.length === 0){
-			var error = "Cannot find book in Google Book store"
-			console.log(error)
-			document.getElementById("modalHeader").innerHTML = error
-			$('#modal1').modal('open')
-			return;
-		}
-		bookInfo = details.items[0].volumeInfo
-		var imageURL = bookInfo.imageLinks.thumbnail
-		var description = bookInfo.description || ""
-		document.getElementById("bookCover").src = imageURL
-		document.getElementById("modalHeader").innerHTML = title
-		document.getElementById("modalDescription").innerHTML = description
-		$('#modal1').modal('open');
-	})
+function openNotes(title, author) {
+  getDetailed(title, author, function(details) {
+    // TODO Check items == 0
+    if (details.items.length === 0) {
+      var error = "Cannot find book in Google Book store";
+      console.log(error);
+      document.getElementById("modalHeader").innerHTML = error;
+      $("#modal1").modal("open");
+      return;
+    }
+    bookInfo = details.items[0].volumeInfo;
+    var imageURL = bookInfo.imageLinks.thumbnail;
+    var description = bookInfo.description || "";
+    document.getElementById("bookCover").src = imageURL;
+    document.getElementById("modalHeader").innerHTML = title;
+    document.getElementById("modalDescription").innerHTML = description;
+    $("#modal1").modal("open");
+  });
 }
 
-function getRandomMateralizeColor(){
-	var values = {
-		color:["red","purple","deep-purple","indigo","purple","blue","light-blue","cyan","teal","green","orange","deep-orange"],
-		brightness:["lighten","darken"],
-		intensity:["","-1","-2","-3"] // -4 can be too light
-	}
-	var color = values.color[Math.floor(Math.random()*values.color.length)]
-	var brightness = values.brightness[Math.floor(Math.random()*values.brightness.length)]
-	var intensity = values.intensity[Math.floor(Math.random()*values.intensity.length)]
-	return color + " " + brightness + intensity
+function getRandomMateralizeColor() {
+  var values = {
+    color: [
+      "red",
+      "purple",
+      "deep-purple",
+      "indigo",
+      "purple",
+      "blue",
+      "light-blue",
+      "cyan",
+      "teal",
+      "green",
+      "orange",
+      "deep-orange"
+    ],
+    brightness: ["lighten", "darken"],
+    intensity: ["", "-1", "-2", "-3"] // -4 can be too light
+  };
+  var color = values.color[Math.floor(Math.random() * values.color.length)];
+  var brightness =
+    values.brightness[Math.floor(Math.random() * values.brightness.length)];
+  var intensity =
+    values.intensity[Math.floor(Math.random() * values.intensity.length)];
+  return color + " " + brightness + intensity;
 }
 
-function fetchJSON(url, callback){
-		var xhttp = new XMLHttpRequest();
-	  	xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	    	try{
-	    		callback(JSON.parse(this.responseText))
-	    	}
-	    	catch(e){
-	    		console.log("Unable to process response: " + this.responseText)
-	    		callback({})
-	    	}
-	    }
-		};
-		xhttp.open("GET", url, true);
-		xhttp.send();
-	}
+function fetchJSON(url, callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      try {
+        callback(JSON.parse(this.responseText));
+      } catch (e) {
+        console.log("Unable to process response: " + this.responseText);
+        callback({});
+      }
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
